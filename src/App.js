9a5ce12a10f0860491330c5af7f06f9d00a8bf09
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-02-07 18:23:56 +0600

    Add EditorForm to App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {Switch, Route} from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./containers/Home/Home";
-// import EditorForm from "./components/EditorForm/EditorForm";
+import EditorForm from "./components/EditorForm/EditorForm";
 
 class App extends Component {
     render() {
@@ -10,7 +10,7 @@ class App extends Component {
             <Layout>
                 <Switch>
                     <Route path="/" exact component={Home}/>
-                    {/*<Route path="/pages/admin" component={EditorForm}/>*/}
+                    <Route path="/pages/admin" component={EditorForm}/>
                     <Route path="/pages/:id" component={Home}/>
                 </Switch>
             </Layout>
