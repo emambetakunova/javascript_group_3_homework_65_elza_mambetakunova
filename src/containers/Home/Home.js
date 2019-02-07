72176179aedbf403a1a0b0dad2084e3e2bf94dd3
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-02-07 12:19:51 +0600

    add containers and components

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import NavigationItems from "../../components/Navigation/NavigationItems/NavigationItems";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <NavigationItems/>
+            </div>
+        );
+    }
+}
+
+export default Home;
