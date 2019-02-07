bb58edb2a2781201684d873849861c1071e156cc
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-02-07 17:19:18 +0600

    refactor code

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,12 +1,49 @@
 import React, {Component} from 'react';
-import NavigationItems from "../../components/Navigation/NavigationItems/NavigationItems";
+import axios from '../../axios-pages';
+
+import Spinner from "../../components/UI/Spinner/Spinner";
 
 class Home extends Component {
+    state = {
+        page: null,
+        loading: true
+    };
+
+
+    getData() {
+
+        let url = 'pages.json';
+        let pageId = this.props.match.params.id;
+        if (!pageId) {
+            pageId = 'home'
+        }
+
+        axios.get(url).then(response => {
+            const { data } = response;
+            const obj = data[pageId];
+            this.setState({loading: false, page: obj});
+        });
+
+    }
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.match.params.id !== prevProps.match.params.id) {
+            this.getData();
+        }
+    }
+
     render() {
         return (
-            <div>
-                <NavigationItems/>
-            </div>
+            this.state.loading ?
+                <Spinner/> :
+                <div className="Home">
+                    <h2>{this.state.page.title}</h2>
+                    <p>{this.state.page.content}</p>
+                </div>
         );
     }
 }
